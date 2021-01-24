c4c2e7cf68660a0d57aa04d5327266b2d7176f46
sacusa <sudhanshugupta96@gmail.com>
2021-01-23 22:44:17 -0500

    Update sweeps

--- edge_tracking_spad/sweep.sh
+++ edge_tracking_spad/sweep.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
 declare -a lane=("1" "2" "4" "8" "16")
-declare -a spad_size=("2" "4" "8" "16" "32" "64")
 declare -a spad_part=("1" "2" "4" "8" "16")
+
+# = SPAD size in KB =   2.00    4.00    7.97    16.00   32.00   64.00
+declare -a spad_size=(  "2"     "4"     "8"     "16"    "32"    "64")
+declare -a spad_width=( "29"    "31"    "50"    "63"    "90"    "127")
+declare -a spad_height=("16"    "31"    "39"    "63"    "89"    "127")
 
 mkdir -p sweep
 
 for l in "${lane[@]}"; do
-    for ss in "${spad_size[@]}"; do
+    for ((ss=0;ss<="${#spad_size[@]}";ss++)); do
         for sp in "${spad_part[@]}"; do
-            ./set_config ${l} ${ss} ${sp}
+            ./set_config ${l} ${spad_width[${ss}]} ${spad_height[${ss}]} ${sp}
 
             sh run.sh
-            mv stdout.gz sweep/stdout_l_${l}_ss_${ss}_sp_${sp}.gz
+            mv stdout.gz sweep/stdout_l_${l}_ss_${spad_size[${ss}]}_sp_${sp}.gz
         done
     done
 done
--- elem_matrix_spad/sweep.sh
+++ elem_matrix_spad/sweep.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
 declare -a lane=("1" "2" "4" "8" "16")
-declare -a spad_size=("2064" "4128" "8256" "16512" "33024" "66048")
 declare -a spad_part=("1" "2" "4" "8" "16")
+
+# = SPAD size in KB =   1.97    3.98    7.99    15.98   31.99   63.98
+declare -a spad_size=(  "2"     "4"     "8"     "16"    "32"    "64")
+declare -a spad_width=( "12"    "17"    "31"    "31"    "65"    "65")
+declare -a spad_height=("7"     "10"    "11"    "22"    "42"    "42")
 
 mkdir -p sweep
 
 for l in "${lane[@]}"; do
-    for ss in "${spad_size[@]}"; do
+    for ((ss=0;ss<="${#spad_size[@]}";ss++)); do
         for sp in "${spad_part[@]}"; do
-            ./set_config ${l} ${ss} ${sp}
+            ./set_config ${l} ${spad_width[${ss}]} ${spad_height[${ss}]} ${sp}
 
             sh run.sh
-            mv stdout.gz sweep/stdout_l_${l}_ss_$((ss/1024))_sp_${sp}.gz
+            mv stdout.gz sweep/stdout_l_${l}_ss_${spad_size[${ss}]}_sp_${sp}.gz
         done
     done
 done
